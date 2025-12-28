da3ba9bd6803339608c5735c3126f81d6977c844
Markas Klimovas <markasklimovas@gmail.com>
2025-12-28 19:00:05 +0200

    migrate to ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,3 +9,4 @@ export default defineConfig({
     strictPort: true
   }
 })
+
